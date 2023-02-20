a31d48ec57ccc15a676c29cb73414da8eccd34cd
Ht <htcvhs971@gmail.com>
2023-02-20 11:20:50 +0100

    Init partie

--- script.js
+++ script.js
@@ -3,23 +3,26 @@
 -Une Partie comporte des joueurs, un joueurVainqueur que l'on ne connaît pas par défaut, et d'un tour.
 -Initialisez une partie, ainsi que 3 nouveaux joueurs. */
 class Partie {
-	constructor(gamer, winner, round) {
-		this.gamer = gamer;
-		this.winner = winner;
-		this.round = round;
+	constructor(joueurs, tour, joueurVainqueur) {
+		this.joueurs = joueurs;
+		this.tour = tour;
+		this.joueurVainqueur = null;
 	}
 }
 
 class Joueur {
-	constructor(lastName, firstName) {
+	constructor(lastName, firstName, points, tour) {
 		this.lastName = lastName;
 		this.firstName = firstName;
-		this.value = 100;
+		this.points = 100;
+		this.tour = 0;
 	}
 }
 
-let winner = [];
+const perso1 = new Joueur('DOE', 'John', 100, 0);
+const perso2 = new Joueur('HALL', 'Berry', 100, 0);
+const perso3 = new Joueur('JOHANSSON', 'Scarlett', 100, 0);
 
-const perso1 = new Joueur('DOE', 'John');
-const perso2 = new Joueur('HALL', 'Berry');
-const perso3 = new Joueur('JOHANSSON', 'Scarlett');
+const partie = new Partie(3, [perso1, perso2, perso3]);
+
+console.log(partie);
