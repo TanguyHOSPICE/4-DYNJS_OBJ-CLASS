1aab83a98381251d0a6cd35e44680acaf791a141
Ht <htcvhs971@gmail.com>
2023-02-20 12:26:15 +0100

    Method attaque

--- script.js
+++ script.js
@@ -17,6 +17,25 @@ class Joueur {
 		this.points = 100;
 		this.tour = 0;
 	}
+	rand(nb) {
+		return Math.floor(Math.random() * Math.floor(nb));
+	}
+	attaque(adversaire) {
+		if (this.tour < 3) {
+			adversaire.points -= this.rand(51);
+			if (adversaire.points < 0) {
+				adversaire.points = 0;
+			}
+			adversaire.affichePoints();
+			++this.tour;
+		}
+	}
+	identite() {
+		return `${this.nom} ${this.prenom}`;
+	}
+	affichePoints() {
+		console.log(`${this.identite()} possède ${this.points} points`);
+	}
 }
 
 const perso1 = new Joueur('DOE', 'John', 100, 0);
@@ -25,4 +44,8 @@ const perso3 = new Joueur('JOHANSSON', 'Scarlett', 100, 0);
 
 const partie = new Partie(3, [perso1, perso2, perso3]);
 
-console.log(partie);
+console.log(perso1);
+/**Modifiez la classe Joueur afin d'implémenter les méthodes suivantes :
+-Une méthode permettant de retourner l'identité du joueur (son nom et son prénom).
+-Une méthode permettant d'afficher les points du joueur, qui appellera la méthode précédente et affichera le nombre de points du joueur.
+-Une méthode permettant à un joueur d'attaquer : une attaque peut faire perdre aléatoirement entre 1 et 50 points à un adversaire. Si l'adversaire n'a plus de points, son total ne peut être négatif, il vaudra simplement zéro. Un joueur dispose de 3 tours, chaque attaque augmente le nombre de tours consommés. */
